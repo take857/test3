46dc9b78e42c4e6bc2da73cf5273442c62262575
ja <takettttt@gmail.com>
2025-12-09 17:20:56 +0900

    Refactor create_db function for clarity

--- creiate_db.py
+++ creiate_db.py
@@ -4,28 +4,28 @@ import requests
 import sqlite3
 import pandas as pd
 
+def create_db():
+    url = "https://raw.githubusercontent.com/lerocha/chinook-database/master/ChinookDatabase/DataSources/Chinook_Sqlite.sql"
 
-url = "https://raw.githubusercontent.com/lerocha/chinook-database/master/ChinookDatabase/DataSources/Chinook_Sqlite.sql"
+    r = requests.get(url)
 
-r = requests.get(url)
+    with open("Chinook_Sqlite.sql", "wb") as f:
+        f.write(r.content)
 
-with open("Chinook_Sqlite.sql", "wb") as f:
-    f.write(r.content)
+    # SQLiteデータベースのファイル名
+    db_file = 'Chinook.db'
+    sql_file = 'Chinook_Sqlite.sql'
 
-# SQLiteデータベースのファイル名
-db_file = 'Chinook.db'
-sql_file = 'Chinook_Sqlite.sql'
+    # データベース接続を開く
+    with sqlite3.connect(db_file) as conn:
+        cursor = conn.cursor()
 
-# データベース接続を開く
-with sqlite3.connect(db_file) as conn:
-    cursor = conn.cursor()
+        # SQLファイルを読み込む
+        with open(sql_file, 'r', encoding='utf-8') as file:
+            sql_script = file.read()
 
-    # SQLファイルを読み込む
-    with open(sql_file, 'r', encoding='utf-8') as file:
-        sql_script = file.read()
+        # SQLスクリプトを実行する
+        cursor.executescript(sql_script)
 
-    # SQLスクリプトを実行する
-    cursor.executescript(sql_script)
-
-    # コミットしてデータベースを保存
-    conn.commit()
+        # コミットしてデータベースを保存
+        conn.commit()
