67c04f4e73c97f683270973a341f706c8377a144
ja <takettttt@gmail.com>
2025-12-09 15:13:54 +0900

    Initialize chat history with default message

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def search(history):
 
 # 画面を構築するためのコード
 if "history" not in st.session_state:
-    st.session_state["history"] = []
+    st.session_state["history"] = ["DBについて回答します。"]
 
 # チャット履歴を表示する
 for message in st.session_state.history:
